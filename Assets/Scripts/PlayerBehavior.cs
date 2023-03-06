539f3f234e4f9dbaa7b61288c07ebdabbe4d1242
Harsimarjit <simarbadding@gmail.com>
2023-03-05 21:48:30 -0500

    Chest

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,11 +14,18 @@ public class PlayerBehavior : MonoBehaviour
     public float groundRadius;
     public LayerMask groundLayerMask;
     public bool isGrounded;
-  //  public PlayerAnimationState animationState;
+    public bool isDucking;
+    public PlayerAnimationState animationState;
 
     private Rigidbody2D rigidbody2D;
     private Animator animator;
 
+    public static bool chestTrigger = false;
+    private bool onceTrigger = true;
+    public GameObject OpenChestObj;
+    public GameObject CloseChestObj;
+    public GameObject starObj;
+    public Transform starSpawnPoint;
 
     // Start is called before the first frame update
     void Start()
@@ -25,7 +33,13 @@ public class PlayerBehavior : MonoBehaviour
         rigidbody2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
+    private void Update()
+    {
+        float z = Input.GetAxis("Vertical");
+        Duck(z);
 
+        OpenChest();
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
@@ -39,11 +53,41 @@ public class PlayerBehavior : MonoBehaviour
         Jump(y);
         AirCheck();
     }
+    private void OpenChest()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && chestTrigger && onceTrigger)
+        {
+            OpenChestObj.SetActive(true);
+            CloseChestObj.SetActive(false);
+            StartCoroutine("SpawnStar", 0.1f);
+            onceTrigger= false;
+        }
+    }
+    IEnumerator SpawnStar(float waitTime)
+    {
+        while (true)
+        {
+            Instantiate(starObj, starSpawnPoint);
+            starObj.GetComponent<Rigidbody2D>().AddForce(Vector2.up * verticalForce * 2f, ForceMode2D.Impulse);
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+    private void Duck(float z)
+    {
+        if (z != 0f)
+            isDucking = true;
+        else
+             isDucking = false;
+        animator.SetBool("isDucking", isDucking);
+    }
     private void Move(float x)
     {
         rigidbody2D.AddForce(Vector2.right * x * horizontalForce * ((isGrounded) ? 1 : airFactor));
 
         rigidbody2D.velocity = new Vector2(Mathf.Clamp(rigidbody2D.velocity.x, -maxSpeed, maxSpeed), rigidbody2D.velocity.y);
+
+        animator.SetFloat("xSpeed", rigidbody2D.velocity.x);
+        animator.SetFloat("ySpeed", rigidbody2D.velocity.y * -1);
 
         if (isGrounded)
         {
@@ -51,11 +95,13 @@ public class PlayerBehavior : MonoBehaviour
             {
               //  animationState = PlayerAnimationState.RUN;
                // animator.SetInteger("AnimationState", (int)animationState);
+
             }
             else
             {
-              //  animationState = PlayerAnimationState.IDLE;
-              //  animator.SetInteger("AnimationState", (int)animationState);
+                //  animationState = PlayerAnimationState.IDLE;
+                //  animator.SetInteger("AnimationState", (int)animationState);
+                animator.SetBool("isGrounded", true);
             }
         }
     }
@@ -70,10 +116,10 @@ public class PlayerBehavior : MonoBehaviour
     {
         if (!isGrounded)
         {
-
             //play jump animation
-          //  animationState = PlayerAnimationState.JUMP;
-          //  animator.SetInteger("AnimationState", (int)animationState);
+            //animationState = PlayerAnimationState.JUMP;
+            //  animator.SetInteger("AnimationState", (int)animationState);
+            animator.SetBool("isGrounded", false);
         }
     }
     private void Flip(float x)
